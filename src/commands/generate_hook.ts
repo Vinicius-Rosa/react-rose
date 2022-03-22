fb7afd9db9af3de545f4b7cbdc3e1c1eb94a2c27
Vinicius Rosa <viniciusrosarafaeldeoliveira@gmail.com>
2022-03-21 21:13:21 -0300

    feat: create custom hook generator

---
+++ src/commands/generate_hook.ts
@@ -0,0 +1,32 @@
+import { GluegunToolbox } from 'gluegun'
+
+module.exports = {
+  name: 'generate:hook',
+  alias: ['g:hook'],
+  run: async (toolbox: GluegunToolbox) => {
+    const {
+      parameters,
+      template: { generate },
+      print,
+    } = toolbox
+
+    const name = parameters.first
+
+    if (!name) {
+      return print.error('Custom hook name is required!')
+    }
+
+    const upperCasedFirstLetter = name[0].toUpperCase()
+    const restStr = name.substring(1)
+    const formattedName = 'use' + upperCasedFirstLetter + restStr
+
+    print.info(`Generating custom hook...`)
+    await generate({
+      template: 'useHook.ts.ejs',
+      target: `src/app/hooks/${formattedName}.ts`,
+      props: { name: formattedName },
+    })
+
+    print.success(`Generated component at src/components/${formattedName}!!`)
+  },
+}
